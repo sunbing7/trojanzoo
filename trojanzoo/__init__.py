e6a6adc738018a1fdcb14d3b99c6b9d2321305b3
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-01-10 11:55:56 +0800

    test

--- trojanzoo/__init__.py
+++ trojanzoo/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from .version import __version__ as internal_version
-from torch.torch_version import TorchVersion
+#from torch.torch_version import TorchVersion
 
 from trojanzoo import environ as environ
 from trojanzoo import datasets as datasets
@@ -11,4 +11,4 @@ from trojanzoo import trainer as trainer
 from trojanzoo.utils.module import summary
 
 __all__ = ['summary']
-__version__ = TorchVersion(internal_version)
+__version__ = 0#TorchVersion(internal_version)
